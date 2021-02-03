a18d63d4d893add7fce2c9b1cb728ddc0df2e5d1
zeerakt <45472756+zeerakt@users.noreply.github.com>
2021-02-03 19:16:23 +0500

    minor fixes

--- app/src/main/java/com/rngesus/smartwallet/MainActivity.java
+++ app/src/main/java/com/rngesus/smartwallet/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, QrcodeActivity.class);
-        startActivity(intent);
+       // Intent intent = new Intent(MainActivity.this, QrcodeActivity.class);
+       // startActivity(intent);
 
     }
 }
