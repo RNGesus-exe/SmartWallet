4947273da5c369cdeb564f884e099f58168565e1
rngesus <behzadshaheryar@gmail.com>
2021-02-12 17:40:52 +0500

    Revamped Recharge Activity.

--- app/src/main/java/com/rngesus/smartwallet/RechargeAccount.java
+++ app/src/main/java/com/rngesus/smartwallet/RechargeAccount.java
@@ -1,46 +1,29 @@
 package com.rngesus.smartwallet;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.type.DateTime;
-import com.rngesus.smartwallet.ui.home.HomeFragment;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.Transaction;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Objects;
 
 public class RechargeAccount extends AppCompatActivity {
-    EditText RechargeCode;
-    Button btn;
-    FirebaseAuth mAuth;
-    FirebaseFirestore firebaseFirestore;
-    private String card;
-    String str ="";
-    private String rechargeamount;
-    private Firebase fb = new Firebase();
-    private DataManager dataManager = new DataManager();
 
+    private EditText et_recharge;
+    private FirebaseFirestore db;
+    ArrayList<Cards> pin_list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,32 +32,27 @@ public class RechargeAccount extends AppCompatActivity {
         init();
     }
 
+    public interface MyCallback {
+        void onCallback(ArrayList<Cards> pin_list);
+    }
+
     private void init() {
         getSupportActionBar().setTitle("Recharge");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        RechargeCode = findViewById(R.id.etRechargeCode);
-        btn = findViewById(R.id.recharge);
-        mAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
+        et_recharge = findViewById(R.id.et_recharge);
+        db = FirebaseFirestore.getInstance();
     }
 
     public void Recharge(View view) {
-        DocumentReference docRef;
-        String pin = RechargeCode.getText().toString();
-        fb.loadCardsData(pin,this);
-        docRef =  fb.loadReceiverDocRef(mAuth.getCurrentUser().getEmail(),this);
-
-
-        if (pin.equals("")) {
-            Toast.makeText(this, "Please Enter Recharge Pin ", Toast.LENGTH_SHORT).show();
-        }else
-        {
-            if(fb.pinFlag) {
-                fb.loadCardsData(pin, this);
-                docRef =  fb.loadReceiverDocRef(mAuth.getCurrentUser().getEmail(),this);
-                if(docRef!=null) {
-                    fb.executeCardTransaction(this, docRef);
+        String pin = et_recharge.getText().toString().trim();
+        if(pin.isEmpty()){
+            Toast.makeText(this, "Please enter the pin code first!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            load_cards(pin_loader -> {
+                if(check_card_validation(pin)){
+                    this.perform_transaction(pin);
+                    et_recharge.setText("");
                     Date currentTime = Calendar.getInstance().getTime();
                     String Str = currentTime.toString();
                     String[] allParts = Str.split("\\s+");
@@ -82,19 +60,63 @@ public class RechargeAccount extends AppCompatActivity {
                     String time = allParts[3];
                     DataManager dataManager = new DataManager();
                     dataManager.addIncomeReceipt(FirebaseAuth.getInstance().getCurrentUser().getUid(),
-                            date, time, "Recharge Retailer",
-                            "QR Transfer",String.valueOf(fb.recharge_amount), view, false);
-                    RechargeCode.setText("");
-
+                            date, time, "Recharge Shop",
+                            "QR Transfer",String.valueOf(get_amount(pin)), view, false);
                 }
                 else{
-                    Toast.makeText(this, "Loading Failed please try again ", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RechargeAccount.this, "The entered pin was incorrect!", Toast.LENGTH_SHORT).show();
                 }
-
-            }
-
+            });
         }
 
+    }
+
+    private int get_amount(String pin){
+        for(Cards card: pin_list){
+            if(card.getID().equals(pin)){
+                return card.getRecharge();
+            }
+        }
+        return 0;
+    }
+
+    private void perform_transaction(String pin){
+        db.runTransaction((Transaction.Function<Integer>) transaction -> {
+            DocumentReference user_ref = db.collection("USERS").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            DocumentSnapshot RSnapshot = transaction.get(user_ref);
+            Long updated_user_amount = RSnapshot.getLong("Amount") + (this.get_amount(pin));
+            transaction.update(user_ref, "Amount", updated_user_amount);
+            return null;
+        })
+                .addOnCompleteListener(task -> Toast.makeText(RechargeAccount.this,
+                        "Recharge Complete",Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(RechargeAccount.this,
+                        "Failed Transfer"+ e.getMessage(),Toast.LENGTH_SHORT).show());
+    }
+
+    private boolean check_card_validation(String pin){
+        for(Cards card : pin_list){
+            if(card.getID().equals(pin)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void load_cards(MyCallback callback){
+        db.collection("Cards")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                            Cards card = new Cards(document.getId(),Integer.parseInt(document.get("recharge").toString()));
+                            pin_list.add(card);
+                        }
+                        callback.onCallback(pin_list);
+                    } else {
+                        Toast.makeText(this, "An error occurred while Loading!!", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
 }
